83e1f32cf95b6ede33bf76106a7be8677043f13c
Paul <Paul@paulorized.com>
2026-03-28 11:14:37 -0400

    Fix build: remove orphaned setNearbyDispensaries and setLocationSummary reset calls

--- app/strain-search/page.tsx
+++ app/strain-search/page.tsx
@@ -72,8 +72,6 @@ export default function StrainSearchPage() {
     setError('');
     setResult(null);
     setLogMatches([]);
-    setNearbyDispensaries([]);
-    setLocationSummary('');
     setLocationError('');
     try {
       const res = await fetch('/api/strain-search', {
